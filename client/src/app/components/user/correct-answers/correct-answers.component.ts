73f86259d6e0d3fe843bff1a4efbdccafcf76957
Mozhgan71 <mozhganrajabali@gmail.com>
2024-04-25 13:14:35 +0330

    Add pagination In Correct Answer

--- client/src/app/components/user/correct-answers/correct-answers.component.ts
+++ client/src/app/components/user/correct-answers/correct-answers.component.ts
@@ -1,31 +1,77 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Question } from '../../../models/question.model';
 import { environment } from '../../../../environments/environment.development';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { AdminService } from '../../../services/admin.service';
+import { MemberParams } from '../../../models/helpers/member-params';
+import { Subscription } from 'rxjs';
+import { Pagination } from '../../../models/helpers/pagination';
+import { PaginatedResult } from '../../../models/helpers/paginatedResult';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-correct-answers',
   templateUrl: './correct-answers.component.html',
   styleUrls: ['./correct-answers.component.scss'],
-  imports:[CommonModule]
+  imports: [CommonModule, MatPaginatorModule]
 })
-export class CorrectAnswersComponent {
+export class CorrectAnswersComponent implements OnInit, OnDestroy {
+  adminService = inject(AdminService);
   private readonly baseApiUrl = environment.apiUrl;
-  
+
   private http = inject(HttpClient);
-  
+
   questions: Question[] | undefined;
 
-  constructor() {
+  subscribed: Subscription | undefined;
+  readonly pageSizeOptions = [5, 10, 15, 20];
+  pageEvent: PageEvent | undefined;
+  pagination: Pagination | undefined;
+  memberParams: MemberParams | undefined;
+
+  ngOnInit(): void {
+    this.memberParams = new MemberParams();
+
     this.showAnswerQuestion();
   }
 
+  ngOnDestroy(): void {
+    this.subscribed?.unsubscribe();
+  }
+
   showAnswerQuestion(): void {
-    this.http.get<Question[]>(this.baseApiUrl + 'question/').subscribe(
-      { next: response => this.questions = response }
-    );
+    if (this.memberParams)
+    this.subscribed = this.adminService.getAllQuestions(this.memberParams).subscribe({
+      next: (response: PaginatedResult<Question[]>) => {
+        if (response.body && response.pagination) {
+          this.questions = response.body;
+          console.log("QUESTIONS:", this.questions);
+          this.pagination = response.pagination;
+          console.log("PAGINATION:", this.pagination);
+        }
+      }
+    });
+  }
+
+  // showAnswerQuestion(): void {
+  //   this.http.get<Question[]>(this.baseApiUrl + 'question/').subscribe(
+  //     { next: response => this.questions = response }
+  //   );
+  // }
+
+  handlePageEvent(e: PageEvent) {
+    if (this.memberParams) {
+      if (e.pageSize !== this.memberParams.pageSize)
+        e.pageIndex = 0;
+
+      this.pageEvent = e;
+      this.memberParams.pageSize = e.pageSize;
+      this.memberParams.pageNumber = e.pageIndex + 1;
+
+      this.showAnswerQuestion();
+    }
   }
 
   goTop(): void {
