de6175d2437affd8c52815c9ad8a8c0aa629f2d9
Mozhgan71 <mozhganrajabali@gmail.com>
2024-06-03 00:36:22 +0330

    update tokenService2

--- backend/api/Services/TokenService.cs
+++ backend/api/Services/TokenService.cs
@@ -1,26 +1,42 @@
-namespace api.Services;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
 
+namespace api.Services;
 public class TokenService : ITokenService
 {
+    private readonly IMongoCollection<AppUser> _collection;
     private readonly SymmetricSecurityKey? _key; // set it as nullable by ? mark
+    private readonly UserManager<AppUser> _userManager;
 
-    public TokenService(IConfiguration config)
+    public TokenService(IConfiguration config, IMongoClient client, IMyMongoDbSettings dbSettings, UserManager<AppUser> userManager)
     {
-        string? tokenValue = config["TokenKey"];
+        var database = client.GetDatabase(dbSettings.DatabaseName);
+        // _collection = database.GetCollection<AppUser>(AppVariablesExtensions.collectionUsers);
+
+        // TODO replace these
+        string? tokenValue = config[AppVariablesExtensions.TokenKey];
+        // string? tokenValue = config.GetValue<string>(AppVariablesExtensions.TokenKey);
 
         // throw exception if tokenValue is null
         _ = tokenValue ?? throw new ArgumentNullException("tokenValue cannot be null", nameof(tokenValue));
 
         _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenValue!));
+
+        _userManager = userManager;
     }
 
-    public async Task<string> CreateToken(AppUser user)
+    public async Task<string> CreateToken(AppUser appUser)
     {
         _ = _key ?? throw new ArgumentNullException("_key cannot be null", nameof(_key));
 
         var claims = new List<Claim> {
-            new Claim(JwtRegisteredClaimNames.NameId, user.Id!)
+            new Claim(JwtRegisteredClaimNames.NameId, appUser.Id.ToString())
+            // new Claim(JwtRegisteredClaimNames.Email, appUser.Email)
         };
+
+        // Get user's roles and add them all into claims
+        IList<string>? roles = await _userManager.GetRolesAsync(appUser);
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
         var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
